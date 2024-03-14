e4d279823f85ed3d4ebbe229f32616202aa1da4b
Edouard Misset <edouardmisset@gmail.com>
2024-03-14 19:16:18 +0100

    feat: add averageTime function to calculate average time from an array of Date objects

--- src/math/average.test.ts
+++ src/math/average.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assertThrows } from '@std/assert'
-import { average } from './average.ts'
+import { average, averageTime } from './average.ts'
 
 Deno.test('average function', async (t) => {
   await t.step('with multiple arguments', () => {
@@ -17,4 +17,36 @@ Deno.test('average function', async (t) => {
   await t.step('with no arguments', () => {
     assertThrows(() => average())
   })
+})
+
+Deno.test('averageTime function', async (t) => {
+  await t.step('should return the average time', () => {
+    const dates = [
+      new Date('2022-01-01T09:00:00Z'),
+      new Date('2022-01-01T11:00:00Z'),
+    ]
+    const result = averageTime(dates)
+    assertEquals(result, '10:00:00')
+  })
+
+  await t.step(
+    'should return the correct average time for dates spanning multiple days',
+    () => {
+      const dates = [
+        new Date('2022-01-01T23:00:00Z'),
+        new Date('2022-01-01T01:00:00Z'),
+      ]
+      const result = averageTime(dates)
+      assertEquals(result, '12:00:00')
+    },
+  )
+
+  await t.step(
+    'should return "00:00:00" for an empty array',
+    () => {
+      const dates: Date[] = []
+      const result = averageTime(dates)
+      assertEquals(result, '00:00:00')
+    },
+  )
 })
--- src/math/average.ts
+++ src/math/average.ts
@@ -36,3 +36,38 @@ export function average(...num: (number[] | number)[]): number {
  * Alias for the {@link average} function.
  */
 export const mean: typeof average = average
+
+/**
+ * Calculates the average time from an array of Date objects and returns it as a
+ * string.
+ * 
+ * **Note:** For best results, the dates should be in the same timezone, in the
+ * format ("YYYY-MM-DDTHH:MM:SSZ") `2022-01-01T01:00:00Z` (ISO 8601).
+ * The time is calculated as if all the dates were on the same day.
+ *
+ * @param {Date[]} dates The array of Date objects.
+ * @returns {string} The average time in the format "HH:MM:SS".
+ *
+ * @example
+ * ```typescript
+ * import { averageTime } from './averageTime'
+ *
+ * const dates = [new Date('2022-01-01T09:00:00Z'), new Date('2022-01-01T11:00:00Z')]
+ * averageTime(dates)
+ * // returns "10:00:00"
+ * ```
+ */
+export function averageTime(dates: Date[]): string {
+  if (dates.length === 0) {
+    return '00:00:00'
+  }
+
+  const datesInMs = dates.map((date) => {
+    const newDate = new Date(date)
+    newDate.setUTCFullYear(2000, 0, 1)
+    return newDate.getTime()
+  })
+  const averageDate = new Date(average(datesInMs))
+
+  return averageDate.toISOString().slice(11, 19)
+}
