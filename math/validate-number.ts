ce1ed32ea820f586c0e5601d22c70c1890c73192
Edouard <63284636+edouardmisset@users.noreply.github.com>
2024-05-21 12:25:15 +0200

    feat: add validateNumber function
    * feat: add validateNumber function
    
    * feat: add validateNumber function
    
    * chore: update version to 3.1.0 in deno.json

---
+++ math/validate-number.ts
@@ -0,0 +1,36 @@
+/**
+ * Validates if the input is a number.
+ *
+ * **Note**: `NaN` and `Infinity` are not considered valid numbers.
+ *
+ * @param {unknown} n - The input to validate.
+ * @returns {boolean} Returns true if the input is a number and is finite, false
+ * otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { validateNumber } from './validate-number.ts'
+ *
+ * validateNumber(123)
+ * // returns true
+ *
+ * validateNumber('123')
+ * // returns true
+ *
+ * validateNumber('123abc')
+ * // returns false
+ *
+ * validateNumber(Infinity)
+ * // returns false
+ * ```
+ */
+export function validateNumber(n: unknown): boolean {
+  if (typeof n === 'number') return Number.isFinite(n)
+  if (typeof n === 'string') {
+    // @ts-expect-error: we want to explicitly this test case (for runtime behavior)
+    // deno-lint-ignore eqeqeq
+    return Number.isFinite(parseFloat(n)) && Number(n) == n
+  }
+
+  return false
+}
