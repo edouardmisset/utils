cbdb90c3033e53072114183c83a738053c82d305
Edouard Misset <edouardmisset@gmail.com>
2025-09-23 22:49:22 +0200

    chore: rename parameter name in average function

--- math/average.ts
+++ math/average.ts
@@ -3,7 +3,7 @@ import { err, ok, type Result } from '@edouardmisset/function'
 /**
  * Calculates the average of the given numbers.
  *
- * @param {...(number[] | number)[]} number_ - The numbers to calculate the average of.
+ * @param {...(number[] | number)[]} numbers - The numbers to calculate the average of.
  * @returns {Result<number, Error>} A result object containing either the average or an error.
  *
  * @example
@@ -35,23 +35,22 @@ import { err, ok, type Result } from '@edouardmisset/function'
  * ```
  */
 export function average(
-  ...number_: (number[] | number)[]
+  ...numbers: (number[] | number)[]
 ): Result<number, Error> {
-  const numbers = number_.flat()
+  const nums = numbers.flat()
 
-  if (numbers.length === 0) {
+  if (nums.length === 0) {
     return err(
       new Error(
-        `Cannot calculate average if no values are passed in (${
-          String(number_)
+        `Cannot calculate average if no values are passed in (${String(numbers)
         })`,
       ),
     )
   }
 
   const average =
-    numbers.reduce((accumulator, value) => accumulator + value, 0) /
-    numbers.length
+    nums.reduce((accumulator, value) => accumulator + value, 0) /
+    nums.length
 
   return ok(average)
 }
