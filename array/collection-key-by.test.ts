fdd46f11b01be13d78de1c89f1e31756da49c186
Edouard Misset <edouardmisset@gmail.com>
2025-10-29 15:32:24 +0100

    refactor: separate keyBy function into its own file and add corresponding tests

--- array/collection-key-by.test.ts
+++ array/collection-key-by.test.ts
@@ -1,31 +1,11 @@
 import { assertEquals } from '@std/assert'
-import { collectionKeyBy, keyBy } from './collection-key-by.ts'
+import { collectionKeyBy } from './collection-key-by.ts'
 
 const userCollection = {
   '1': { id: 1, name: 'Alice' },
   '2': { id: 2, name: 'Bob' },
   '3': { id: 3, name: 'Charlie' },
 } as const
-
-Deno.test('keyBy', async (t) => {
-  await t.step(
-    'should transform an array of objects into an object keyed by a specified key',
-    () => {
-      const array = [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }, {
-        id: 3,
-        name: 'Charlie',
-      }]
-      const key = 'id'
-      assertEquals(keyBy(array, key), userCollection)
-    },
-  )
-
-  await t.step('should return undefined for an empty array', () => {
-    const array: Record<string, unknown>[] = []
-    const key = 'id'
-    assertEquals(keyBy(array, key), undefined)
-  })
-})
 
 Deno.test('collectionKeyBy', async (t) => {
   await t.step(
